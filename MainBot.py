13dec96c93ee575eb3fea435122ee5cc71c9280e
Khurshed <khurshed-shavkatzhonovich@yandex.ru>
2024-03-27 15:09:38 +0500

    completed last working code

--- MainBot.py
+++ MainBot.py
@@ -3,7 +3,6 @@ from telethon import TelegramClient, events
 # Данные номеров и id
 api_id = '23671676'
 api_hash = '38a91769321c3e2918fd2efaa8b4faf0'
-phone_numbers = ['+992929438936', '+992501070777']  # Номера телефонов с аккаунтами Telegram
 receiver_id = 7054883903  # ID пользователя, который получает сообщения
 forward_to_id = 749700324  # ID пользователя, на которого пересылаем сообщения
 
@@ -23,7 +22,9 @@ async def main():
         if event.message.sender_id != forward_to_id:
             # Копируем только-что присланное сообщение для receiver_id
             # и отправляем от имени receiver_id на аккаунт forward_to_id
-            await event.message.forward_to(forward_to_id)    
+            await event.message.forward_to(forward_to_id)  
+            await receiver_client.delete_messages(receiver_id, event.message.id)
+            await receiver_client.delete_dialog(forward_to_id)
 
     # Запускаем клиенты
     await receiver_client.run_until_disconnected()
